1ba02d1284190f6a2f47b291c31f7f7814324fb9
DESKTOP-OAU9JF5\PC <jeti11@naver.com>
2023-07-02 22:08:39 +0900

    상단고정 리모컨 ui 구현

--- src/main/java/com/bitc/bmn_project/controller/BaeController.java
+++ src/main/java/com/bitc/bmn_project/controller/BaeController.java
@@ -1,4 +1,21 @@
 package com.bitc.bmn_project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/bmn")
 public class BaeController {
+  @RequestMapping("")
+  public String index() throws Exception {
+    return "index";
+  }
+
+  @RequestMapping(value = "/viewDetail", method = RequestMethod.GET)
+  public ModelAndView viewDetail() throws Exception {
+    ModelAndView mv = new ModelAndView("bmn/viewDetail");
+    return mv;
+  }
 }
